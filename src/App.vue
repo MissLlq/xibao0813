b3edb1a0d63ead69578543337e33afcd9c3666c6
v_liangliqin <v_liangliqin@baidu.com>
2025-08-13 19:37:29 +0800

    add project

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <h1 class="xibao">
+   这是希宝的图片
+  </h1>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import image from '../assets/xibao.jpeg';
+
+</script>
+
+<template>
+  <img :src="image" alt="image" class="xibao" />
+
+  <div class="card">
+  尝试部署第一个vite应用
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+.xibao {
+  width: 60%;
+  height: auto;
+}
+</style>
